a142399fc08e89e8c651fcdbadc03667afca030d
yohanessugiarto <49071377+yohanessugiarto@users.noreply.github.com>
2024-01-17 07:59:34 +0700

    Update templates (#2)
    * Create Django project
    
    * Update templates and add Bootstrap
    
    ---------
    
    Co-authored-by: ysugiarto <96357054+ysugiarto@users.noreply.github.com>
    Co-authored-by: Yohanes <yohanes.sugiarto@awv-informatik.ch>

--- financeapp/settings.py
+++ financeapp/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+load_dotenv('.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "expenses",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = "financeapp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -75,8 +79,18 @@ WSGI_APPLICATION = "financeapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": os.environ.get('DB_ENGINE'),
+        # "NAME": os.environ.get('DB_NAME'),
+        # "USER": os.environ.get('DB_USER'),
+        # "PASSWORD": os.environ.get('DB_PASSWORD'),
+        # "HOST": os.environ.get('DB_HOST'),
+        # "PORT": os.environ.get('DB_PORT')
+        "ENGINE": os.getenv('DB_ENGINE'),
+        "NAME": os.getenv('DB_NAME'),
+        "USER": os.getenv('DB_USER'),
+        "PASSWORD": os.getenv('DB_PASSWORD'),
+        "HOST": os.getenv('DB_HOST'),
+        "PORT": os.getenv('DB_PORT')
     }
 }
 
@@ -115,7 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'financeapp/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
